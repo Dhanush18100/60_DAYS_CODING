8007a742780a9e57d94c1ea42429a5439318da72
Dhanush18100 <dhanush.nayak.100@gmail.com>
2025-11-05 18:18:14 +0530

    Day5

---
+++ vowelsConsonent.java
@@ -0,0 +1,21 @@
+public class vowelsConsonent {
+    public static void main(String[] args) {
+      String str=  "Hello World";
+    count(str);
+    }
+    public static void count(String str){
+        int vowels=0,cons=0;
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+
+            if(ch>'a'&&ch<'z'){
+                 if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+                    vowels++;
+                else
+                    cons++;
+            }
+        }
+        System.out.println("vowels"+vowels);
+        System.out.println("Consonents"+cons);
+    }
+}
