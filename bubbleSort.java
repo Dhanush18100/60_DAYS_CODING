58c12eb47fa498c9f1aeedc981a339636c358507
Dhanush18100 <dhanush.nayak.100@gmail.com>
2025-11-11 16:22:12 +0530

    BubbleSort

---
+++ bubbleSort.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class bubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 1, 4, 2, 8};
+        bubbleSorting(arr);
+        for(int num:arr){
+            System.out.print(num+" ");
+        }
+    }
+    public static void bubbleSorting(int []arr){
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+}
