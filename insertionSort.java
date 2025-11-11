512177aa2cde1db0a08b414e7f5aa45b0dbce251
Dhanush18100 <dhanush.nayak.100@gmail.com>
2025-11-11 18:56:52 +0530

    insertionSort

---
+++ insertionSort.java
@@ -0,0 +1,22 @@
+public class insertionSort {
+    public static void main(String[] args) {
+         int[] arr = {12, 11, 13, 5, 6};
+        insertSort(arr);
+
+        System.out.println("Sorted Array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+    public static void insertSort(int []arr){
+        for(int i=1;i<arr.length;i++){
+            int key=arr[i];
+            int j=i-1;
+            while(j>=0 &&arr[j]>key){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=key;
+        }
+    }
+}
