e36247efc04b4edef90f779e64de5f4530d74b6b
Dhanush18100 <dhanush.nayak.100@gmail.com>
2025-11-13 08:45:34 +0530

    Patterns

---
+++ invertedHalfPyramid.java
@@ -0,0 +1,23 @@
+public class invertedHalfPyramid {
+    public static void main(String[] args) {
+        int n=4;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-1-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=0;k<=i;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n-2;i>=0;i--){
+            for(int j=0;j<n-1-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=0;k<=i;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
