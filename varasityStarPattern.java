5c7d533863bfbb198b4a1cf5587c8fc859937b9e
Dhanush18100 <dhanush.nayak.100@gmail.com>
2025-11-13 12:16:37 +0530

    numberPatterns

---
+++ varasityStarPattern.java
@@ -0,0 +1,17 @@
+public class varasityStarPattern {
+    public static void main(String[] args) {
+        int n=4;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<n;j++){
+                if(i==1||i==n||j==1||j==n-1){
+                    System.out.print("3 ");
+                }else if(i==2 && j==2){
+                    System.out.print("1 ");
+                }else{
+                    System.out.print("2 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
