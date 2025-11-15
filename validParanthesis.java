877aac61913e9e5fdaee2ccb064530a3a39bc540
Dhanush18100 <dhanush.nayak.100@gmail.com>
2025-11-15 10:59:41 +0530

    validParanthesis

---
+++ validParanthesis.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+
+public class validParanthesis {
+    public static void main(String[] args) {
+         String s1 = "()[]{}";
+        String s2 = "([)]";
+        String s3 = "({[]})";
+
+        System.out.println(isValid(s1)); // true
+        System.out.println(isValid(s2)); // false
+        System.out.println(isValid(s3)); // true
+    }
+    public static boolean isValid(String s){
+        Stack<Character> stack=new Stack<>();
+        for(char c:s.toCharArray()){
+            if(c=='(') stack.push(')');
+            else if(c=='{') stack.push('}');
+            else if(c=='[') stack.push(']');
+            else{
+                if(stack.isEmpty()||stack.pop()!=c){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
