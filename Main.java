a30e49eac317da05d08c3e986fdc1733ef391698
Dhanush18100 <dhanush.nayak.100@gmail.com>
2025-11-16 08:56:58 +0530

    longestCommonPrefix

---
+++ Main.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        String s1 = strs[0];
+        String s2 = strs[strs.length - 1];
+        int idx = 0;
+
+        while (idx < s1.length() && idx < s2.length()) {
+            if (s1.charAt(idx) == s2.charAt(idx)) {
+                idx++;
+            } else {
+                break;
+            }
+        }
+        return s1.substring(0, idx);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        String[] arr = {"flower", "flow", "flight"};
+        System.out.println(sol.longestCommonPrefix(arr));
+    }
+}
