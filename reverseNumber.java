ea45f4795b0b514a924feff6241d2f01e027bf4b
Dhanush18100 <dhanush.nayak.100@gmail.com>
2025-11-20 14:27:49 +0530

    reverseANumber

---
+++ reverseNumber.java
@@ -0,0 +1,12 @@
+public class reverseNumber{
+    public static void main(String[] args) {
+        int a=123;
+        int rev=0;
+        while(a!=0){
+            int rem=a%10;
+            rev=rev*10+rem;
+            a/=10;
+        }
+        System.out.println(rev);
+    }
+}
